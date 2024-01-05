9ca12b7f8ac3cc6dfe10109c2f2384d064f26a89
Mauricio Fragale <mfragale@gmail.com>
2024-01-05 14:31:26 -0300

    update

--- includes/js/gf_turmas_start_functions.js
+++ includes/js/gf_turmas_start_functions.js
@@ -2,31 +2,38 @@ jQuery(document).ready(function ($) {
 
     //$('body').css("background-color", "yellow");
 
-    var i = 0;
-    $('.turmas_nova_start option').each(function () {
-        var turma = new Date($(this).val());
-        var hoje = new Date($.now());
-
-        if (turma < hoje || i > 2) {
-            var show = false;
-            $(this).hide();
-        } else {
-            var show = true;
-            $(this).show();
-            i++;
+    $('.gfield_select').each(function () {
+        if ($(this).children().val() == "Pré-inscrição") {
+            return;
         }
 
-        const options = {
-            weekday: "long", year: "numeric", month: "numeric", day: "numeric", hour: "numeric", minute: "numeric"
-        };
-        var newval = new Date($(this).val()).toLocaleString("pt-BR", options);
+        var visible = 1;
+        $(this).children('option').each(function (index) {
 
-        $(this).val(newval);
-        $(this).text(newval);
+            var turma = new Date($(this).val());
+            var hoje = new Date($.now());
 
-        // console.log(turma, ' > ', hoje, ' = ', show);
-        // console.log($(this).val());
-        // console.log(i);
+            const options = {
+                weekday: "long", year: "numeric", month: "numeric", day: "numeric", hour: "numeric", minute: "numeric"
+            };
+            var newval = new Date($(this).val()).toLocaleString("pt-BR", options);
+
+            if (turma < hoje || visible > 4) {
+                $(this).val(newval + " - i=" + index + " - v=" + visible);
+                $(this).text(newval + " - i=" + index + " - v=" + visible);
+                $(this).hide();
+            } else {
+                $(this).val(newval + " - i=" + index + " - v=" + visible);
+                $(this).text(newval + " - i=" + index + " - v=" + visible);
+                visible++;
+            }
+
+        });
+
+        if (visible == 1) {
+            $(this).append('<option selected value="Pré-inscrição">Pré-inscrição</option>');
+        }
+
     });
 
 });
